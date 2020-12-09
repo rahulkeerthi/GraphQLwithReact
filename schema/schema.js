196013f3ddabcddb60aeaaa06c608ccd399a4c05
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-12-09 17:03:03 +0000

    adding companies and association with users

--- schema/schema.js
+++ schema/schema.js
@@ -1,19 +1,37 @@
 import { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } from "graphql"
 // lodash helps with enums traversal
-import _ from "lodash"
+import axios from "axios"
 
-const users = [
-	{ id: "23", firstName: "Bill", age: 20 },
-	{ id: "47", firstName: "Susan", age: 34 },
-]
+// Initial example using hardcoded values
+// const users = [
+// 	{ id: "23", firstName: "Bill", age: 20 },
+// 	{ id: "47", firstName: "Susan", age: 34 },
+// ]
 
-// Defining a node type
+// Defining a node type (Company)
+const CompanyType = new GraphQLObjectType({
+	name: "Company",
+	fields: {
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString },
+	},
+})
+
+// Defining a node type (User)
 const UserType = new GraphQLObjectType({
 	name: "User",
 	fields: {
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
 		age: { type: GraphQLInt },
+		company: {
+			type: CompanyType,
+			args: { id: { type: GraphQLString } },
+			resolve(parentValue, args) {
+				return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`).then(resp => resp.data)
+			},
+		},
 	},
 })
 
@@ -26,7 +44,7 @@ const RootQuery = new GraphQLObjectType({
 			type: UserType,
 			args: { id: { type: GraphQLString } },
 			resolve(_parentValue, args) {
-				return _.find(users, { id: args.id })
+				return axios.get(`http://localhost:3000/users/${args.id}`).then(resp => resp.data)
 			},
 		},
 	},
